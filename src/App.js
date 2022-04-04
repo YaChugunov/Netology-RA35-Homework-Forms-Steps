54c4aa8ad1c6679eb1e97a58040c5beeaf528d30
YaChugunov <informed.looser@gmail.com>
2022-04-04 20:41:18 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,95 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
+
+function StepsFormInput(props) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.dir(e);
+  };
+
+  return (
+    <div class="Steps-FormInput">
+      <form action="" onSubmit={onSubmit}>
+        <input type="text" name="inputDate" />
+        <input type="text" name="inputSteps" />
+        <button>OK</button>
+      </form>
+    </div>
+  );
+}
+
+class StepsRecordsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recorddate: '',
+      recordsteps: '',
+    };
+  }
+
+  // Обрабатываем изменение HEX-кода цвета фона
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const listItems = numbers.map((number) => (
+      <tr>
+        <td>{listitems}</td>
+        <td>{listitems}</td>
+        <td></td>
+      </tr>
+    ));
+  };
+
+  const;
+  render() {
+    return (
+      <>
+        <tr>
+          <td>{this.state.recorddate}</td>
+          <td>{this.state.recordsteps}</td>
+          <td></td>
+        </tr>
+      </>
+    );
+  }
+}
+
+class Steps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <StepsFormInput />
+        <table
+          border="1"
+          cellspacing="0"
+          cellpadding="2"
+          class="Steps-RecordsList"
+        >
+          <thead>
+            <tr>
+              <th>Дата (ДД.ММ.ГГ)</th>
+              <th>Пройдено (км)</th>
+              <th>Действия</th>
+            </tr>
+          </thead>
+          <tbody>
+            <StepsRecordsList />
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Steps />
     </div>
   );
 }
