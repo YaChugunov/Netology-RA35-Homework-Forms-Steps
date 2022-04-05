5716791bc8eba1f451442bdbde15027bca8a4420
YaChugunov <informed.looser@gmail.com>
2022-04-05 15:23:23 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,70 +1,118 @@
 import React, { useState } from 'react';
 import './style.css';
 
-function FormInput() {
-  const [form, setForm] = useState({
-    date: new Date(),
-    steps: 0,
-  });
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault(); // При нажатии ничего не происходит
-    console.dir(name);
-    return <Item date={form.date} steps={form.steps} />;
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+//
+// --- --- --- --- ---
+//
+class FormInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.form = {
+      inputDate: formatDate(new Date()),
+      inputSteps: 0,
+    };
+    this.records = {
+      row: [
+        {
+          id: null,
+          recordDate: null,
+          recordSteps: null,
+        },
+      ],
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.form.inputDate, this.form.inputSteps);
+    this.setState((prevState) => ({
+      records: [
+        ...prevState.records,
+        [this.form.inputDate, this.form.inputSteps],
+      ],
+    }));
+    console.log(this.row);
   };
 
-  const handleInputChange = (e) => {
-    // e.preventDefault(); // При нажатии ничего не происходит
-    const inputName = e.target.name;
-    const type = e.target.type;
+  handleInputChange = (e) => {
+    // e.preventDefault();
+    const name = target.name;
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
 
-    const value = type === 'checkbox' ? e.target.checked : e.target.value;
-
-    setForm((prevState) => ({ ...prevState, [inputName]: value }));
-    console.log(value, type);
+    this.setState({
+      [name]: value,
+    });
   };
 
-  return (
-    <>
-      <form action="" onSubmit={handleFormSubmit}>
-        <div className="form">
-          <div className="formDate">
-            <label htmlFor="date">Дата (ДД.ММ.ГГ)</label>
-            <input name="date" type="date" onChange={handleInputChange} />
+  render() {
+    const data = [{ name: 'test1' }, { name: 'test2' }];
+    const listItems = data.map((d) => (
+      <tr key={d.name}>
+        <td>{d.name}</td>
+        <td></td>
+        <td></td>
+      </tr>
+    ));
+
+    return (
+      <>
+        <form action="" onSubmit={this.handleFormSubmit}>
+          <div className="form">
+            <div className="formDate">
+              <label htmlFor="date">Дата (ДД.ММ.ГГ)</label>
+              <input
+                name="inputDate"
+                type="date"
+                value={this.inputDate}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="formSteps">
+              <label htmlFor="steps">Пройдено (км)</label>
+              <input
+                name="inputSteps"
+                type="number"
+                value={this.inputSteps}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div>
+              <button type="submit">OK</button>
+            </div>
           </div>
-          <div className="formSteps">
-            <label htmlFor="steps">Пройдено (км)</label>
-            <input
-              name="steps"
-              type="number"
-              value={form.steps}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <button type="submit">OK</button>
-          </div>
-        </div>
-      </form>
-      <table
-        border="1"
-        cellSpacing="0"
-        cellPadding="2"
-        className="Steps-RecordsList"
-      >
-        <thead>
-          <tr>
-            <th>Дата (ДД.ММ.ГГ)</th>
-            <th>Пройдено (км)</th>
-            <th>Действия</th>
-          </tr>
-        </thead>
-        <tbody>
-          <ItemList />
-        </tbody>
-      </table>
-    </>
-  );
+        </form>
+        <table
+          border="1"
+          cellSpacing="0"
+          cellPadding="2"
+          className="Steps-records"
+        >
+          <thead>
+            <tr>
+              <th>Дата (ДД.ММ.ГГ)</th>
+              <th>Пройдено (км)</th>
+              <th>Действия</th>
+            </tr>
+          </thead>
+          <tbody>{listItems}</tbody>
+        </table>
+      </>
+    );
+  }
 }
 
 class Item extends React.Component {
@@ -73,30 +121,26 @@ class Item extends React.Component {
     this.state = {};
   }
   render() {
-    return (
-      <>
-        <tr>
-          <td>{this.props.date}</td>
-          <td>{this.props.steps}</td>
-          <td></td>
-        </tr>
-      </>
+    const item = (
+      <tr>
+        <td>{this.props.date}</td>
+        <td>{this.props.steps}</td>
+        <td></td>
+      </tr>
     );
+    return <>{item}</>;
   }
 }
 
 class ItemList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      date: this.props.date,
-      steps: this.props.steps,
-    };
+    this.state = {};
   }
   render() {
     return (
       <>
-        <Item date={this.state.date} steps={this.state.steps} />
+        <Item date={this.props.date} steps={this.props.steps} />
       </>
     );
   }
