a49508d04670c87d77cf4d5789d52992ae937f1f
YaChugunov <informed.looser@gmail.com>
2022-04-05 11:01:33 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,87 +1,66 @@
 import React, { useState } from 'react';
 import './style.css';
 
-class StepsFormInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: '',
-      steps: '',
-    };
+function FormInput() {
+  const [form, setForm] = useState({
+    date: '',
+    steps: null,
+  });
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.value : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  render() {
-    return (
-      <form>
-        <div className="form">
-          <div className="formDate">
-            <label htmlFor="date">Дата (ДД.ММ.ГГ)</label>
-            <input name="date" type="date" onChange={this.handleInputChange} />
-          </div>
-          <div className="formSteps">
-            <label>Пройдено (км)</label>
-            <input
-              name="steps"
-              type="number"
-              value={this.state.steps}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <button type="submit">OK</button>
-          </div>
-        </div>
-      </form>
-    );
-  }
-}
-
-class StepsRecordsList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recorddate: '',
-      recordsteps: '',
-    };
-  }
-
-  // Обрабатываем изменение HEX-кода цвета фона
-  onSubmit = (e) => {
-    e.preventDefault();
-
-    const listItems = items.map((item) => (
-      <tr>
-        <td>{listitems}</td>
-        <td>{listitems}</td>
-        <td></td>
-      </tr>
-    ));
+  const handleFormSubmit = (e) => {
+    e.preventDefault(); // При нажатии ничего не происходит
+    console.dir(name);
   };
 
-  const;
-  render() {
-    return (
-      <>
-        <tr>
-          <td>{this.state.recorddate}</td>
-          <td>{this.state.recordsteps}</td>
-          <td></td>
-        </tr>
-      </>
-    );
-  }
+  const handleInputChange = (e) => {
+    // e.preventDefault(); // При нажатии ничего не происходит
+    const inputName = e.target.name;
+    const type = e.target.type;
+
+    const value = type === 'checkbox' ? e.target.checked : e.target.value;
+
+    setForm((prevState) => ({ ...prevState, [inputName]: value }));
+    console.log(value, type);
+  };
+
+  return (
+    <form action="" onSubmit={handleFormSubmit}>
+      <div className="form">
+        <div className="formDate">
+          <label htmlFor="date">Дата (ДД.ММ.ГГ)</label>
+          <input name="date" type="date" onChange={handleInputChange} />
+        </div>
+        <div className="formSteps">
+          <label htmlFor="steps">Пройдено (км)</label>
+          <input
+            name="steps"
+            type="number"
+            value={form.steps}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <button type="submit">OK</button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+function Item(props) {
+  const [item, setItem] = useState({
+    id: '',
+    date: true,
+    steps: null,
+  });
+  const item = (
+
+  );
+  return <>{item}</>;
+}
+function ItemList() {
+
+  return <>{}</>;
 }
 
 class Steps extends React.Component {
@@ -93,7 +72,7 @@ class Steps extends React.Component {
   render() {
     return (
       <>
-        <StepsFormInput />
+        <FormInput />
         <table
           border="1"
           cellSpacing="0"
@@ -108,7 +87,7 @@ class Steps extends React.Component {
             </tr>
           </thead>
           <tbody>
-            <StepsRecordsList />
+            <ItemList />
           </tbody>
         </table>
       </>
